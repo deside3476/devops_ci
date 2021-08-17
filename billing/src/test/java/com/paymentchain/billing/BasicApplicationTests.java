ca4d088ada44c1a5c34ceb4a67be17f445fae2f5
jaad <jaad2323@domai.com>
2021-08-16 22:35:16 -0600

    se agrega ficheros a repositorio

---
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -0,0 +1,18 @@
+package com.paymentchain.billing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+        string meessage= "default message cambio test devops";
+        Assert.assertNotNull(meessage);
+	}
+
+}
